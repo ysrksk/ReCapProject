855ec8373192eb74ec11cd33b3f9329204657f89
Yaşar Küskü <yasarkusku@hotmail.com>
2022-07-13 09:20:11 +0300

    api kurulumu

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //ICarService _carService;
+        //public CarsController(ICarService carService)
+        //{
+        //    _carService = carService;
+        //}
+        CarManager carManager = new CarManager(new EfCarDal());
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = carManager.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
